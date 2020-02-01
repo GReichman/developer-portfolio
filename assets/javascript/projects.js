37053557dc2c181787856cc0669cb1eba472ad46
Garrett Reichman <gsreichman@gmail.com>
2020-01-31 22:20:52 -0500

    finished portfolio page

---
+++ assets/javascript/projects.js
@@ -0,0 +1,20 @@
+const projects = [
+    {
+        name: "Rick and Morty site",
+        repo: "https://github.com/rforde1/Rick-and-Morty",
+        live: "https://rforde1.github.io/Rick-and-Morty/",
+        image: "assets/pictures/home.jpg"
+    },
+    {
+        name: "City Weather App",
+        repo: "https://github.com/GReichman/weather-dashboard",
+        live: "https://greichman.github.io/weather-dashboard/",
+        image: "assets/pictures/weather.png"
+    },
+    {
+        name: "Day Planner",
+        repo: "https://github.com/GReichman/day-planner",
+        live: "https://greichman.github.io/day-planner/",
+        image: "assets/pictures/planner.png"
+    }
+];
